d4454aba1ea7dc022541daadcb5923c227920469
apixcel <admin@apixcel.com>
2024-06-07 17:35:53 +0600

    f

--- index.js
+++ index.js
@@ -10,7 +10,9 @@ const doctorRoutes = require("./routes/doctorRoutes");
 const cors = require("cors");
 const app = express();
 
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ extended: false}))
+
 app.use(cors());
 // Connect to MongoDB
 mongoose
