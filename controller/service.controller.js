40bed3bb9e35bb3e32b79f0a5bc7f5892424688f
apixcel <admin@apixcel.com>
2024-06-08 12:54:09 +0600

    g

--- controller/service.controller.js
+++ controller/service.controller.js
@@ -1,17 +1,20 @@
-const Service = require('../Models/Service.model');
+const Service = require("../Models/Service.model");
 
 // Create and Save a new Service
 exports.create = async (req, res) => {
   try {
     // Validate request
-    if (!req.body.title || !req.body.description) {
-      return res.status(400).send({ message: "Title and description cannot be empty!" });
+    if (!req.body.title || !req.body.description || !req.body.uri) {
+      return res
+        .status(400)
+        .send({ message: "Title and description and uri cannot be empty!" });
     }
 
     // Create a Service
     const service = new Service({
       title: req.body.title,
       description: req.body.description,
+      uri: req.body.uri,
     });
 
     // Save Service in the database
@@ -19,19 +22,20 @@ exports.create = async (req, res) => {
     res.status(201).json(savedService);
   } catch (error) {
     res.status(500).json({
-      message: error.message || "Some error occurred while creating the Service."
+      message:
+        error.message || "Some error occurred while creating the Service.",
     });
   }
 };
 
-
 exports.findAll = async (req, res) => {
-    try {
-      const services = await Service.find();
-      res.status(200).json(services);
-    } catch (error) {
-      res.status(500).json({
-        message: error.message || "Some error occurred while retrieving services."
-      });
-    }
-  };
+  try {
+    const services = await Service.find();
+    res.status(200).json(services);
+  } catch (error) {
+    res.status(500).json({
+      message:
+        error.message || "Some error occurred while retrieving services.",
+    });
+  }
+};
